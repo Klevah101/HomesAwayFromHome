cbe759813a8678df8cfe0ae1912ec654e53da042
Nsilo Brown <NsiloBrown@hotmail.com>
2024-01-21 14:34:35 -0500

    start phase 4

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,7 +5,7 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-
+////////////remove these routes //////////////////
 router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
     res.cookie("XSRF-TOKEN", csrfToken);
@@ -16,5 +16,5 @@ router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('Hello World!');
 });
-
+///////////////////////////////////////////////////
 module.exports = router;
