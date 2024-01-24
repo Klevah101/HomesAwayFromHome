a20f67538116dde609ae1fd82f1ebc5a9c402ce2
Klevah101 <nsilobrown@hotmail.com>
2024-01-24 18:12:53 -0500

    added validation to session route

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -2,12 +2,26 @@ const express = require('express');
 const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
 
-router.post('/', async (req, res, next) => {
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+  ];
+
+router.post('/', validateLogin, async (req, res, next) => {
     const { credential, password } = req.body;
 
     const user = await User.unscoped().findOne({
@@ -23,9 +37,9 @@ router.post('/', async (req, res, next) => {
     // newObj.User = user.dataValues;
     // console.log(newObj);
 
-    let jsonUser = JSON.stringify(user);
-    jsonUser = JSON.parse(jsonUser);
-    console.log(jsonUser)
+    // let jsonUser = JSON.stringify(user);
+    // jsonUser = JSON.parse(jsonUser);
+    // console.log(jsonUser)
 
     // console.log(typeof user)
 
