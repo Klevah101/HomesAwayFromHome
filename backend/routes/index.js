8b7608abc5958d8efecf3b776687836d760c8f69
Klevah101 <nsilobrown@hotmail.com>
2024-03-08 19:39:46 -0500

    updated csrf cookie for use with other browsers

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -30,7 +30,9 @@ if (process.env.NODE_ENV === 'production') {
 // Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== 'production') {
   router.get('/api/csrf/restore', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.cookie('XSRF-TOKEN', req.csrfToken(),
+      // { sameSite: 'None', secure: true } // enable to be able to sign in on non chrome browsers
+    );
     return res.json({});
   });
 }
